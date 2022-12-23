a53bc054f29719e581628d2302cf2e073a2df77c
Stephen Giovanni Saputra <stephen.gsaputra@gmail.com>
2022-12-23 12:04:14 +0700

    Initial Commit

---
+++ MovieAPI/Views/MovieListVC.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  MovieAPI
+//
+//  Created by Stephen Giovanni Saputra on 23/12/22.
+//
+
+import UIKit
+
+class MovieListVC: UIViewController {
+
+    // MARK: - Properties
+    
+    
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        configureUI()
+    }
+    
+    // MARK: - Selectors
+    
+    
+    // MARK: - Helpers
+    func configureUI() {
+        
+        view.backgroundColor = .systemGray
+    }
+}
+
